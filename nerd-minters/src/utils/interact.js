2a6138f7732a56c6da3661e69cc34483864388ca
Ataxia123 <vp.ca94@gmail.com>
2022-03-31 09:24:44 -0500

    nerds rule

--- nerd-minters/src/utils/interact.js
+++ nerd-minters/src/utils/interact.js
@@ -1,10 +1,12 @@
 import { pinJSONToIPFS } from "./pinata.js";
-require("dotenv").config();
+require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const {createAlchemyWeb3} = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
 const contractABI = require("../contract-abi.json");
 const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey);
+
 
 export const connectWallet = async () => {
   if (window.ethereum) {
